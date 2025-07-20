d6862474d7131e31213b098bc8398df3cf4d57b6
BL00DY-C0D3 <151839541+BL00DY-C0D3@users.noreply.github.com>
2025-07-20 18:13:11 +0200

    Update do_backup.sh

--- do_backup.sh
+++ do_backup.sh
@@ -4,7 +4,11 @@ CUSTOM_HOST_STRING=""
 if [ ! -z "$CUSTOM_HOST" ]; then
     CUSTOM_HOST_STRING="--backup-id $CUSTOM_HOST"
 fi
+NAMESPACE_STRING=""
+if [ ! -z "$NAMESPACE" ]; then
+    NAMESPACE_STRING="--ns $NAMESPACE"
+fi
 
 echo "------------------------------------------------------------"
-proxmox-backup-client backup $BACKUP_TARGETS $CUSTOM_HOST_STRING
+proxmox-backup-client backup $BACKUP_TARGETS $CUSTOM_HOST_STRING $NAMESPACE_STRING
 echo "------------------------------------------------------------"
